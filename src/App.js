43a6a2bfa8f8c2122c42234b5a55c670ee5ce552
Edswel <franckini@yahoo.com>
2021-03-25 21:33:36 +0100

    Hard code movie to test API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MovieList from './components/MovieList';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hey...</h1>
-    </div>
-  );
+const App = () => {
+
+
+  const [movies, setMovies] = useState([
+    {
+      Title: "The Avengers",
+      Year: "2012",
+      imdbID: "tt0848228",
+      Type: "movie",
+      Poster: "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"
+    },
+    {
+      Title: "Avengers: Infinity War",
+      Year: "2018",
+      imdbID: "tt4154756",
+      Type: "movie",
+      Poster: "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+    },
+    {
+      Title: "Avengers: Endgame",
+      Year: "2019",
+      imdbID: "tt4154796",
+      Type: "movie",
+      Poster: "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg"
+    }
+  ]);
+  return <div>
+    <MovieList movies={movies} />
+  </div>
 }
 
 export default App;
